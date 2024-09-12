2e4740c0dbd1898e06dd8fe070431c3a02c3142d
Philenej <philene.j@gmail.com>
2024-09-12 16:59:17 +0200

    2de poging

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
 console.log("hi");
+
+// menu sluiten en openen
+// let deButton = document.querySelector("nav button");
+// let deUl = document.querySelector("nav");
+
+// deButton.onclick = toggleMenu;
+
+// function toggleMenu(){
+//     deUl.classList.toggle("toonMenu");
+// }
+
+// document.querySelector('.menu-button').addEventListener('click', function() {
+//     document.querySelector('nav').classList.toggle('toonMenu');
+//   });
+
+//   document.querySelector('nav button').addEventListener('click', function() {
+//     document.querySelector('nav').classList.remove('toonMenu');
+//   });
