6ae7ad281ab0c3b33f5b617bdffe0bc7967c9ac3
Philenej <philene.j@gmail.com>
2024-09-13 11:08:01 +0200

    top

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,21 @@ console.log("hi");
 //     deUl.classList.toggle("toonMenu");
 // }
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuButton = document.querySelector('.menu-button');
+//     const nav = document.querySelector('nav');
+  
+//     // Menu open/close togglen
+//     menuButton.addEventListener('click', () => {
+//       nav.classList.toggle('toonMenu');
+//     });
+  
+//     // Sluit button functionaliteit
+//     const closeButton = nav.querySelector('button');
+//     closeButton.addEventListener('click', () => {
+//       nav.classList.remove('toonMenu');
+//     });
+//   });
 // document.querySelector('.menu-button').addEventListener('click', function() {
 //     document.querySelector('nav').classList.toggle('toonMenu');
 //   });
@@ -18,3 +33,23 @@ console.log("hi");
 //   document.querySelector('nav button').addEventListener('click', function() {
 //     document.querySelector('nav').classList.remove('toonMenu');
 //   });
+
+/****************************************/
+/* menu openen en sluiten met de button */
+/****************************************/
+
+/* JOUW CODE HIER - stap 4 */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+let deButton = document.querySelector("nav button");
+let deUl = document.querySelector("nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+deButton.onclick = toggleMenu;
+
+
+// stap 3: voeg in de functie een class toe aan de nav
+
+function toggleMenu(){
+  deUl.classList.toggle("toonMenu");
+}
