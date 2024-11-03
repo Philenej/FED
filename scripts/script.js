cd598c3546f145ccdbb5e4d7513823a04e8ec757
Philenej <philene.j@gmail.com>
2024-11-03 16:55:47 +0100

    oke

--- scripts/script.js
+++ scripts/script.js
@@ -18,16 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
   //  alle knoppen die een dropdown effect moeten hebben
   document.querySelectorAll('section > h2 > button').forEach(function(button) {
     button.addEventListener("click", function() {
-      // Zoek het paragraaf element binnen dezelfde section als de knop
+
       const details = button.closest('section').querySelector('p'); 
       
-      // Wissel de zichtbaarheid van het paragraaf element
+
       if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block"; // Toon de details
-        button.querySelector('span').textContent = "-"; // Verander het symbool naar "-"
+        button.querySelector('span').textContent = "-"; // Verander naar -
       } else {
-        details.style.display = "none"; // Verberg de details
-        button.querySelector('span').textContent = "+"; // Verander het symbool naar "+"
+        details.style.display = "none"; 
+        button.querySelector('span').textContent = "+"; // Verander naar +
       }
     });
   });
